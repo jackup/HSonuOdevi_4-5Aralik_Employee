3dda37b102d32ebfbae16d473cd0e01195262f7e
yakup <yakup@COEN-JACKUP>
2021-12-05 16:38:08 +0300

    Proje dosyası ekle.

---
+++ HSonuOdevi_4-5Aralik_Employee/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSonuOdevi_4_5Aralik_Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
